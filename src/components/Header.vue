76b59ed311f65e4237cf6884c3fcfc6101b630e7
Abhijeet Sawant <sawantabhie@gmail.com>
2022-02-02 23:48:08 +0530

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <nav id="navbar" class="row">
+            <div class="logo">
+                <span class="q-mx-sm">☰</span>
+                <span>The Ninja Market</span>
+            </div>
+            <input type="text" class="q-mx-xl q-px-lg borderless text-body2" placeholder="Search">
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+#navbar {
+    background: #0995e7;
+    padding: 0.3rem;
+}
+
+.logo {
+    color: white;
+}
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 import router from './router'
+import './quasar'
 
 Vue.config.productionTip = false
+
+Vue.use(VueResource)
 
 new Vue({
   router,
